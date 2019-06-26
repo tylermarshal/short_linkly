9aa3268c079c557a79c84f73014152cd54090102
Tyler Madsen <tyler.madsen@travelershaven.com>
2019-06-26 09:27:28 -0600

    Add decoded_id spec

--- spec/models/short_link_spec.rb
+++ spec/models/short_link_spec.rb
@@ -12,16 +12,20 @@ RSpec.describe ShortLink, type: :model do
     let(:long_link) { 'https://www.google.com' }
     let!(:short_link) { create(:short_link, long_link: long_link, user_id: 1) }
 
-    context 'user_id is same' do
+    context 'short link already exists with user_id' do
       subject { build(:short_link, long_link: long_link, user_id: 1) }
 
-      it { is_expected.to_not be_valid }
+      it 'is not valid' do
+        is_expected.to_not be_valid
+      end
     end
 
-    context 'user_id is different' do
+    context 'short link does not yet exist with user_id' do
       subject { build(:short_link, long_link: long_link, user_id: 2) }
 
-      it { is_expected.to be_valid }
+      it 'is valid' do
+        is_expected.to be_valid
+      end
     end
   end
 
@@ -31,21 +35,31 @@ RSpec.describe ShortLink, type: :model do
     end
   end
 
-  describe '.find_by_encoded_id' do
-    let(:short_link) { create(:short_link) }
-    subject { described_class.find_by_encoded_id(short_link.encoded_id) }
-
-    it 'returns the matching short_link' do
-      is_expected.to eq(short_link)
-    end
-  end
-
   describe '#encoded_id' do
     let(:short_link) { create(:short_link) }
     subject { short_link.encoded_id }
 
     it 'returns a base 36 encoded id' do
       is_expected.to eq(short_link.id.to_s(36))
+    end
+  end
+
+  describe '.decoded_id' do
+    let(:short_link) { create(:short_link) }
+    let(:short_link_encoded_id) { short_link.encoded_id }
+    subject { described_class.decoded_id(short_link_encoded_id) }
+
+    it 'decodes a base 36 encoded id' do
+      is_expected.to eq(short_link_encoded_id.to_i(36))
+    end
+  end
+
+  describe '.find_by_encoded_id' do
+    let(:short_link) { create(:short_link) }
+    subject { described_class.find_by_encoded_id(short_link.encoded_id) }
+
+    it 'returns the matching short_link' do
+      is_expected.to eq(short_link)
     end
   end
 end
