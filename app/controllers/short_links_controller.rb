14a50449b88a6b4ffcdb6244a586c7d59b706617
Tyler Madsen <tyler.madsen@travelershaven.com>
2019-06-26 09:35:24 -0600

    Remove code to be added

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -5,28 +5,10 @@ class ShortLinksController < ApplicationController
     redirect_to @short_link.long_link, status: :moved_permanently
   end
 
-  def create
-    short_link = ShortLink.find_or_initialize_by(short_link_params)
-
-    if short_link.persisted? || short_link.save
-      render json: { long_link: short_link.long_link, short_link: shortened_link(short_link) }, status: :created
-    else
-      render json: short_link.errors, status: :unprocessable_entity
-    end
-  end
-
   private
 
   def set_short_link
     @short_link = ShortLink.find_by_encoded_id(params[:id])
     head :not_found unless @short_link
-  end
-
-  def short_link_params
-    params.permit(:long_link, :user_id)
-  end
-
-  def shortened_link(short_link)
-    "#{request.base_url}/#{short_link.encoded_id}"
   end
 end
--- db/migrate/20190516022749_create_short_links.rb
+++ db/migrate/20190516022749_create_short_links.rb
@@ -1,8 +1,7 @@
 class CreateShortLinks < ActiveRecord::Migration[5.2]
   def change
     create_table :short_links do |t|
-      t.string :long_link, null: false, index: { unique: true }
-
+      t.string :long_link, null: false
       t.timestamps
     end
   end
